fc9dc0de25ba73a1f7d1c40b0ccb4eb40b101878
mark novelero <mark.novelero@gmail.com>
2021-05-18 21:08:38 -0400

    commit tree 880a381a3c9d08e1caff6701e1df251a376e3e59

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,30 @@
-# Make your shoe class here!
+
+class Shoe
+    attr_reader :color, :size, :material, :condition
+    attr_reader :brand
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def color=(color)
+        @color= color
+    end
+
+    def size=(num)
+        @size= num
+    end
+
+    def material=(material)
+        @material = material
+    end
+
+    def condition=(condition)
+        @condition= condition
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
